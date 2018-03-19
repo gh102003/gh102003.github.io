25d5d13322d1ca6a3a41c30939b75026d300d588
George <george@pyesmeadow.com>
2018-03-19 20:08:50 +0000

    Refactor init code

--- js/navigation.js
+++ js/navigation.js
@@ -1,21 +1,21 @@
-$(document).ready(function () {
+function init()
+{
+    alert("a");
 
-  alert("a");
+    $(".btn-toggle-menu").click(function() {
 
-  $(".btn-toggle-menu").click(function() {
+      if($(".directory").is(".open")) {
+        $(".directory-wrapper").slideUp({"duration": 600, "easing": "easeInOutCubic", "queue": false});
+        $(".directory").animate({"top": "-10.9vw"}, {"duration": 600, "easing": "easeInOutCubic", "queue": false});
+        // $(".directory").css("top", "-11vw");
+        $(".btn-toggle-menu").css("transform", "scale(1, 1)");
+      } else {
+        $(".directory-wrapper").slideDown({"duration": 600, "easing": "easeInOutCubic", "queue": false});
+        $(".directory").animate({"top": "0"}, {"duration": 600, "easing": "easeInOutCubic", "queue": false});
+        // $(".directory").css("top", "0");
+        $(".btn-toggle-menu").css("transform", "scale(1, -1)");
+      }
 
-    if($(".directory").is(".open")) {
-      $(".directory-wrapper").slideUp({"duration": 600, "easing": "easeInOutCubic", "queue": false});
-      $(".directory").animate({"top": "-10.9vw"}, {"duration": 600, "easing": "easeInOutCubic", "queue": false});
-      // $(".directory").css("top", "-11vw");
-      $(".btn-toggle-menu").css("transform", "scale(1, 1)");
-    } else {
-      $(".directory-wrapper").slideDown({"duration": 600, "easing": "easeInOutCubic", "queue": false});
-      $(".directory").animate({"top": "0"}, {"duration": 600, "easing": "easeInOutCubic", "queue": false});
-      // $(".directory").css("top", "0");
-      $(".btn-toggle-menu").css("transform", "scale(1, -1)");
-    }
-
-    $(".directory").toggleClass("open");
-  })
-})
+      $(".directory").toggleClass("open");
+    })
+}
