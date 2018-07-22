8fc91d77e8072a7ccaea3c77a42140e29fcb9744
George Howarth <george@pyesmeadow.com>
2018-07-22 11:52:12 +0100

    Tweak spacing and hover effects in directory

--- js/navigation.js
+++ js/navigation.js
@@ -17,12 +17,10 @@ function init()
     if(!$(".directory").is(".open")) {
       $(".directory-wrapper").slideUp({"duration": 600, "easing": "easeInOutCubic", "queue": false});
       $(".directory").animate({"top": "-10.9vw"}, {"duration": 600, "easing": "easeInOutCubic", "queue": false});
-      // $(".directory").css("top", "-11vw");
       $(".btn-toggle-menu").css("transform", "scale(1, 1)");
     } else {
       $(".directory-wrapper").slideDown({"duration": 600, "easing": "easeInOutCubic", "queue": false});
       $(".directory").animate({"top": "0"}, {"duration": 600, "easing": "easeInOutCubic", "queue": false});
-      // $(".directory").css("top", "0");
       $(".btn-toggle-menu").css("transform", "scale(1, -1)");
     }
   })
