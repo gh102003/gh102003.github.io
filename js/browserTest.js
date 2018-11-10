23fcd1bf5bd0ad4612107bf59a3917f843fc6f59
George Howarth <george@pyesmeadow.com>
2018-11-10 13:23:34 +0000

    Decrease sixe of header and check browser compatability

---
+++ js/browserTest.js
@@ -0,0 +1,7 @@
+function browserTest() {
+    var ua = window.navigator.userAgent;
+    
+    if (ua.indexOf("MSIE") > 0 ||  ua.indexOf("Trident") > 0) {
+        alert("Internet Explorer is not supported. If you are using Windows 10, you can use Microsoft Edge, but otherwise you must use Google Chrome or Mozilla Firefox");
+    }
+}
