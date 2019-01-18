fcf8aa75d68d13563ba7742600d34783727ad604
George Howarth <george@pyesmeadow.com>
2019-01-18 20:16:08 +0000

    Add hover effect to nav button

--- js/navigation.js
+++ js/navigation.js
@@ -1,17 +1,35 @@
-function initNavigation()
-{
-  console.log($(".btn-toggle-menu"));
-  $(".btn-toggle-menu").click(function() {
-    $(".directory").toggleClass("open");
+function initNavigation() {
+    $(".btn-toggle-menu").click(function () {
+        $(".directory").toggleClass("open");
 
-    if(!$(".directory").is(".open")) {
-      $(".directory-wrapper").slideUp({"duration": 500, "easing": "easeInOutCubic", "queue": false});
-      $(".directory").animate({"top": "-10.9vw"}, {"duration": 500, "easing": "easeInOutCubic", "queue": false});
-      $(".btn-toggle-menu").css("transform", "scale(1, 1)");
-    } else {
-      $(".directory-wrapper").slideDown({"duration": 500, "easing": "easeInOutCubic", "queue": false});
-      $(".directory").animate({"top": "0"}, {"duration": 500, "easing": "easeInOutCubic", "queue": false});
-      $(".btn-toggle-menu").css("transform", "scale(1, -1)");
-    }
-  })
+        if (!$(".directory").is(".open")) {
+            $(".directory-wrapper").slideUp({
+                "duration": 500,
+                "easing": "easeInOutCubic",
+                "queue": false
+            });
+            $(".directory").animate({
+                "top": "-10.9vw"
+            }, {
+                "duration": 500,
+                "easing": "easeInOutCubic",
+                "queue": false
+            });
+            $(".btn-toggle-menu").css("transform", "rotateX(0deg)");
+        } else {
+            $(".directory-wrapper").slideDown({
+                "duration": 500,
+                "easing": "easeInOutCubic",
+                "queue": false
+            });
+            $(".directory").animate({
+                "top": "0"
+            }, {
+                "duration": 500,
+                "easing": "easeInOutCubic",
+                "queue": false
+            });
+            $(".btn-toggle-menu").css("transform", "rotateX(-180deg)");
+        }
+    })
 }
