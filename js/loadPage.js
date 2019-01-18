0008f26e49161abf7297b1ac4007678abe327617
George Howarth <george@pyesmeadow.com>
2019-01-18 20:51:43 +0000

    Fix tabs and introduce sidebar loading system

--- js/loadPage.js
+++ js/loadPage.js
@@ -1,8 +1,12 @@
 function loadPage() {
-    let headerPromise =  new Promise(function(resolve, reject) {
+    let headerPromise = new Promise(function(resolve, reject) {
         $(".header").load("/header.html", null, resolve);
     });
 
+    let contentRightPromise = new Promise(function(resolve, reject) {
+        $(".right").load("/content_right.html", null, resolve);
+    });
+
     // Make sure everything has loaded
-    return Promise.all([headerPromise]);
+    return Promise.all([headerPromise, contentRightPromise]);
 }
