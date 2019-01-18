6a44f88c8b15ef0b9ae8dc079b348d2d99573c9f
George Howarth <george@pyesmeadow.com>
2019-01-18 19:53:31 +0000

    Add more pages

--- js/navigation.js
+++ js/navigation.js
@@ -2,18 +2,15 @@ function initNavigation()
 {
   console.log($(".btn-toggle-menu"));
   $(".btn-toggle-menu").click(function() {
-    console.log("clcik");
-    
-
     $(".directory").toggleClass("open");
 
     if(!$(".directory").is(".open")) {
-      $(".directory-wrapper").slideUp({"duration": 600, "easing": "easeInOutCubic", "queue": false});
-      $(".directory").animate({"top": "-10.9vw"}, {"duration": 600, "easing": "easeInOutCubic", "queue": false});
+      $(".directory-wrapper").slideUp({"duration": 500, "easing": "easeInOutCubic", "queue": false});
+      $(".directory").animate({"top": "-10.9vw"}, {"duration": 500, "easing": "easeInOutCubic", "queue": false});
       $(".btn-toggle-menu").css("transform", "scale(1, 1)");
     } else {
-      $(".directory-wrapper").slideDown({"duration": 600, "easing": "easeInOutCubic", "queue": false});
-      $(".directory").animate({"top": "0"}, {"duration": 600, "easing": "easeInOutCubic", "queue": false});
+      $(".directory-wrapper").slideDown({"duration": 500, "easing": "easeInOutCubic", "queue": false});
+      $(".directory").animate({"top": "0"}, {"duration": 500, "easing": "easeInOutCubic", "queue": false});
       $(".btn-toggle-menu").css("transform", "scale(1, -1)");
     }
   })
