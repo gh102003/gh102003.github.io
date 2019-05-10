f4ae2fe22be5c2752213389f1166bf033519ae02
George Howarth <george@pyesmeadow.com>
2019-05-10 21:17:17 +0100

    More content

--- js/browserTest.js
+++ js/browserTest.js
@@ -1,7 +1,9 @@
 function browserTest() {
     var ua = window.navigator.userAgent;
     
-    if (ua.indexOf("MSIE") > 0 ||  ua.indexOf("Trident") > 0) {
+    if (ua.indexOf("MSIE") > 0 || ua.indexOf("Trident") > 0) {
+        console.log("Bad browser");
+        
         alert("Internet Explorer is not supported. If you are using Windows 10, you can use Microsoft Edge, but otherwise you must use Google Chrome or Mozilla Firefox");
     }
 }
